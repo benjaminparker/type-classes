13d583054234d80b4e6e2f69027cc17ea9719749
benjaminparker <benjamin@needstyping.com>
2019-07-05 00:29:33 +0100

    More realistic example using hierarchical structure

--- src/main/scala/CacheUpdate.scala
+++ src/main/scala/CacheUpdate.scala
@@ -6,39 +6,62 @@ object CacheUpdate {
     def id: String = UUID.randomUUID().toString
   }
 
-  trait Cacheable[T <: Identifiable] {
-    def save(t: T): T
+  trait Cacheable[T] {
+    def save(id: String, t: T): Either[String, T]
 
     def find(id: String): Either[String, T]
   }
 
+  case class Declaration(warehouseInfo: WarehouseInfo, officeOfExit: OfficeOfExit) extends Identifiable
   case class WarehouseInfo(warehouseName: String) extends Identifiable
   case class OfficeOfExit(officeName: String) extends Identifiable
 
-  trait DeclarationCache[T <: Identifiable] {
+  trait DeclarationCache[I <: Identifiable] {
 
-    var store: Map[String, T] = Map.empty
+    var store: Map[String, I] = Map.empty
 
-    def findInCache(id: String): Either[String, T] = Right(store.getOrElse(id, Left("Cannot find it")))
+    def findInCache(id: String): Either[String, I] = store.get(id) match {
+      case Some(foundItem) => Right(foundItem)
+      case None => Left("Not found")
+    }
 
-    def storeInCache(t: T) = {
-      store = store + (t.id -> t)
-      t
+    def storeInCache(i: I) = {
+      store = store + (i.id -> i)
+      i
     }
   }
 
   object CacheableThings {
 
-    implicit object CacheableWarehouseInfo extends Cacheable[WarehouseInfo] with DeclarationCache[WarehouseInfo] {
-      def save(w: WarehouseInfo): WarehouseInfo = storeInCache(w)
-      def find(id: String): Either[String, WarehouseInfo] = findInCache(id)
+    implicit object CacheableWarehouseInfo extends Cacheable[WarehouseInfo] with DeclarationCache[Declaration] {
+      def save(id: String, w: WarehouseInfo): Either[String, WarehouseInfo] = {
+        for {
+          declaration <- findInCache(id)
+          _ = storeInCache(declaration.copy(warehouseInfo = w))
+        } yield w
+      }
+
+      def find(id: String): Either[String, WarehouseInfo] = {
+        for {
+          declaration <- findInCache(id)
+        } yield declaration.warehouseInfo
+      }
     }
 
-    implicit object CacheableOfficeOfExit extends Cacheable[OfficeOfExit] with DeclarationCache[OfficeOfExit] {
-      def save(o: OfficeOfExit): OfficeOfExit = storeInCache(o)
-      def find(id: String): Either[String, OfficeOfExit] = findInCache(id)
-    }
+    implicit object CacheableOfficeOfExit extends Cacheable[OfficeOfExit] with DeclarationCache[Declaration] {
+      def save(id: String, o: OfficeOfExit): Either[String, OfficeOfExit] = {
+        for {
+          declaration <- findInCache(id)
+          _ = storeInCache(declaration.copy(officeOfExit = o))
+        } yield o
+      }
 
+      def find(id: String): Either[String, OfficeOfExit] = {
+        for {
+          declaration <- findInCache(id)
+        } yield declaration.officeOfExit
+      }
+    }
   }
 
   object UpdateService {
@@ -46,20 +69,24 @@ object CacheUpdate {
       implicitly[Cacheable[T]].find(id)
     }
 
-    def updateSomething[T: Cacheable](t: T): T = {
-      implicitly[Cacheable[T]].save(t)
+    def updateSomething[T: Cacheable](id: String, t: T): Either[String,T] = {
+      implicitly[Cacheable[T]].save(id, t)
     }
   }
 
   import CacheableThings._
   val myWarehouse = WarehouseInfo("GB3378ASDW")
-  UpdateService.updateSomething(myWarehouse)
+  UpdateService.updateSomething("someId", myWarehouse)
 
   val myOffice = OfficeOfExit("Birmingham Airport")
-  UpdateService.updateSomething(myOffice)
+  UpdateService.updateSomething("someId", myOffice)
 
-  UpdateService.retrieveSomething("ABCD1234") match {
+  UpdateService.retrieveSomething[WarehouseInfo]("someId") match {
     case Right(warehouseInfo) => displayWarehouse(warehouseInfo)
+    case Left(msg) => error(msg)
+  }
+
+  UpdateService.retrieveSomething[OfficeOfExit]("someId") match {
     case Right(officeOfExit) => displayOfficeOfExit(officeOfExit)
     case Left(msg) => error(msg)
   }
